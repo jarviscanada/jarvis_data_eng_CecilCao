2803b31e1b8afff73d154b3cdea9c6156610f741
Cecil Cao <pokemonball34@gmail.com>
2026-03-31 17:40:03 +0000

    Quick Comments to host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -23,9 +23,9 @@ cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk -F: '{print $2}' | xa
 cpu_mhz=$(echo "$cpu_model" | awk -F@ '{gsub(/[a-zA-Z]/, ""); print $2*1000}' | xargs)  # Assume cpu_model refresh rate is in GHz
 l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S") # current timestamp in `YYYY-MM-DD HH:MM:SS`
-total_mem=$(vmstat -s | egrep "total memory" | awk '{print $1}')
+total_mem=$(vmstat -s --unit M | egrep "total memory" | awk '{print $1}') # Store memory in megabytes
 
-# Add a new entry into the database
+# Add a new entry into host_info table
 insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) VALUES('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, '$timestamp', $total_mem);"
 export PGPASSWORD=$psql_password
 psql -h "$psql_host" -p $psql_port -d "$db_name" -U "$psql_user" -c "$insert_stmt"
